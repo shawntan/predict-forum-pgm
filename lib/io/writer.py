2471825bf3d60bd672835d275b558acfb9030ed7
Shawn Tan <shawn@wtf.sg>
2012-10-08 18:47:13 +0800

    Modifications to learn topics: now can iterate between different sizes.

--- lib/io/writer.py
+++ lib/io/writer.py
@@ -8,32 +8,43 @@ import __builtin__
 
 class FileWrapper(object):
 	def __init__(self,obj):
-		self.obj = obj
-	
+		self._obj = obj
+
 	def close(self,*args,**kwargs):
 		print "Closing file..."
-		self.obj.close(*args,**kwargs)
+		self._obj.close(*args,**kwargs)
 
-	def __defattr__(self,attr):
-		if hasattr(self.obj, attr):
-			attr_value = getattr(self.obj,attr)
-			if isinstance(attr_value,types.MethodType):
-				def callable(*args, **kwargs):
-					return attr_value(*args, **kwargs)
-				return callable
-			else:
-				return attr_value
-		else:
-			raise AttributeError
-
+	def __getattr__(self, attr):
+		# see if this object has attr
+		# NOTE do not use hasattr, it goes into
+		# infinite recurrsion
+		if attr in self.__dict__:
+			# this object has it
+			return getattr(self, attr)
+		# proxy to the wrapped object
+		return getattr(self._obj, attr)
 
 def marked_open(*params):
 	global _open
 	#print params
 	if len(params) > 1 and (params[1] == 'w' or params[1] == 'wb' or params[1] == 'w+'):
 		print "Opening file..."
-	return _open(*params)
+		return FileWrapper(_open(*params))
+	else:
+		return _open(*params)
 
 _open = __builtin__.open
 __builtin__.open = marked_open
-
+"""
+def __defattr__(self,attr):
+	if hasattr(self.obj, attr):
+		attr_value = getattr(self.obj,attr)
+		if isinstance(attr_value,types.MethodType):
+			def callable(*args, **kwargs):
+				return attr_value(*args, **kwargs)
+			return callable
+		else:
+			return attr_value
+	else:
+		raise AttributeError
+"""
